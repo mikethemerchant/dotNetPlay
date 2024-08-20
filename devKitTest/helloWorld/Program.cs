b0185f0089c5d7cffa4f303dd08fc48059e55d06
Michael Bender <mbender@jeldwen.com>
2024-08-20 08:14:58 -0400

    push this

--- devKitTest/helloWorld/Program.cs
+++ devKitTest/helloWorld/Program.cs
@@ -1,3 +1,26 @@
-﻿// from dev kit install tutorial
-Console.WriteLine("Hello, Mike.  You are the guy.");
-Console.WriteLine("The current time is:" + DateTime.Now);
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+        static void Main()
+        {
+            string folderPath = @"C:\Temp\Tow Reports";
+            var csv = new StringBuilder();
+            string filePath = @"C:\Temp\list.csv";
+            
+
+            string[] files = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                csv.AppendLine(file);
+            }
+
+            File.WriteAllText(filePath, csv.ToString());
+
+            Console.WriteLine("done");
+        }
+    
+}
--- devKitTest/helloWorld/obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
+++ devKitTest/helloWorld/obj/Debug/net8.0/helloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f62b4222cbdd849ad131345bfafddd2c583a28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a5a1ef712ede553554606d03847e371a9561230")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
